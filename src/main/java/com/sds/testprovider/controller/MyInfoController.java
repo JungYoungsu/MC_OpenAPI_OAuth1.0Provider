46590e9e002fe31b85e1d241a63f54325b6c77ee
JungYoungsu <terun707@naver.com>
2019-02-21 15:04:17 +0900

    MyInfoContoller - Resource

--- src/main/java/com/sds/testprovider/controller/MyInfoController.java
+++ src/main/java/com/sds/testprovider/controller/MyInfoController.java
@@ -18,20 +18,20 @@ import com.sds.testprovider.service.UsersService;
 
 @Controller
 public class MyInfoController {
-	
+
 	@Autowired
 	private UsersService usersService;
-	
-	@Resource(name="jsonTemplate")
+
+	@Resource(name = "jsonTemplate")
 	private View jsonView;
 
-	@Resource(name="xmlTemplate")
+	@Resource(name = "xmlTemplate")
 	private View xmlView;
 
 	@Autowired
 	private ConsumerService consumerService;
-	
-	@RequestMapping(value="/myinfo")
+
+	@RequestMapping(value = "/myinfo")
 	public ModelAndView getMyInfo(HttpServletRequest request) throws Exception {
 		ModelAndView mav = new ModelAndView();
 		try {
@@ -43,10 +43,18 @@ public class MyInfoController {
 		}
 		return mav;
 	}
-	
-	//OAuthToken이 유효하다면 접근 가능!
+
+	// OAuthToken이 유효하다면 접근 가능!
 	private UsersVO validateOAuthToken(HttpServletRequest request) throws Exception {
-		//아래 return 문을 주석 처리하고 코드를 작성합니다.
-		return null;
+		// 아래 return 문을 주석 처리하고 코드를 작성합니다.
+		OAuthTokenParam param = new OAuthTokenParam(request);
+
+		long userNo = param.getUserNo();
+		String consumerKey = param.getConsumerKey();
+		UsersVO usersVO = usersService.selectUserByUserNo(userNo);
+		ConsumerVO consumerVO = consumerService.selectByConsumerKey(consumerKey);
+		param.validateRequestToken(consumerVO.getConsumerSecret(), usersVO.getPassword());
+
+		return usersVO;
 	}
 }
